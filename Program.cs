778db2d3aed61da23212f5772715b18e191a8c37
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 13:31:29 +0100

    Ex 02 boucles

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int message = 1;
+            while (i < 10)
+            {
+                Console.WriteLine($"Bonjour, je suis le message n° {message} !");
+                i++;
+                message++;
+            }
+        }
+    }
+}
